bcd4c8c20d3fccd28a87ba6d6331374b6d141e65
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-05-31 09:29:12 +0200

    configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js, jsx, ts, tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#008000",
+        dark: {
+          light:"#32CD32",
+          hard: "#006400",
+          soft: "#008000",
+        },
+      },
+      fontFamily:{
+        opensans: ["'Open Sans'", "sans-serif"],
+        roboto: ["'Roboto'", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
