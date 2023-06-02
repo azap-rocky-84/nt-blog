b4a929954f797e2db5d380f7c5d217f2dd67c9b6
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-02 16:33:38 +0200

    fix some comments container bugs

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -11,7 +11,7 @@ const CommentsForm = ({btnLabel, formSubmitHandler, formCancelHandler = null, in
     <form onSubmit={submitHandler}>
       <div className='flex flex-col items-end border border-primary rounded-lg p-4'>
         <textarea className='w-full focus:outline-none bg-transparent' rows="5" placeholder='Lascia un commento ...' value={value} onChange={(e) => setvalue(e.target.value)}/>
-        <div className='flex items-center gap-x-2 pt-2'>
+        <div className='flex flex-col-reverse gap-y-2 items-center gap-x-2 pt-2 min-[420px]:flex-row'>
           {formCancelHandler && (
             <button onClick={formCancelHandler} className='px-6 py-2.5 rounded-lg border border-red-500 text-red-500'>
               Cancella
