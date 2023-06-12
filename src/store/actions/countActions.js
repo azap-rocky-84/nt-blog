a177eb67b40feb89710cd936ac4bae7bff961c34
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-12 20:27:34 +0200

    add logout functionality

--- src/store/actions/countActions.js
+++
@@ -1,9 +0,0 @@
-import { countActions } from "../reducers/countReducers";
-export const changeCount = (type) => (dispatch, getState) =>{
-    const {count} = getState();
-    if(type==='INCREMENTA'){
-        dispatch(countActions.countChange(count.number+1));
-    } else {
-        dispatch(countActions.countChange(count.number-1));
-    }
-};
