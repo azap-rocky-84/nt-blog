dab4958fd9c7ea4b7f61a808d74694e54dea0b8f
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-16 15:56:16 +0200

    add links to navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
 import {MdKeyboardArrowDown} from 'react-icons/md';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { images } from '../constants';
 import { logout } from '../store/actions/user';
 const NavItemsInfo = [
-    {name: "Home", type:"link"},
-    {name: "Articoli", type:"link"},
-    {name: "Pagine", type:"dropdown", items: ["Chi siamo?", "Contattaci"]},
-    {name: "Scopri di più", type:"link"},
-    {name: "FAQ", type:"link"},
+    {name: "Home", type:"link", href:"/"},
+    {name: "Articoli", type:"link", href:"/articles"},
+    {name: "Pagine", type:"dropdown", items: [{title: "Chi siamo?", href:"/about"}, {title: "Contattaci?", href:"/contact"}]},
+    {name: "Scopri di più", type:"link", href:"/more"},
+    {name: "FAQ", type:"link", href:"/faq"},
 ]
 const NavItem = ({item}) =>{
     const [dropdown, setDropdown]= useState(false);
@@ -23,9 +23,9 @@ const NavItem = ({item}) =>{
         <li className='relative group'>
         {item.type === "link" ? ( 
             <>
-            <a href="/" className='px-4 py-2'>
+            <Link to={item.href} className='px-4 py-2'>
             {item.name}
-            </a>
+            </Link>
             <span className='cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
             /</span>
             </> 
@@ -38,9 +38,9 @@ const NavItem = ({item}) =>{
                   <div className={`${dropdown ? "block" : 'hidden'} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
                         <ul className='bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden'>
                             {item.items.map((page, index) => (
-                                    <a key={index} href="/" className='hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft'>
-                                        {page}
-                                    </a>
+                                    <Link key={index} to={page.href} className='hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft'>
+                                        {page.title}
+                                    </Link>
                             ))}
                         </ul>
                   </div>  
@@ -66,9 +66,9 @@ const logoutHandler = () =>{
 }
   return <section className='sticky top-0 left-0 right-0 z-50 bg-white'>
     <header className='container mx-auto px-5 flex justify-between items-center py-4'>
-        <div>
+        <Link to="/">
             <img className='w-8' src={images.Logo} alt="logo" />
-        </div>
+        </Link>
         <div className='lg:hidden z-50'>
             {navIsVisible ? <AiOutlineClose className='w-6 h-6' onClick={navVisibilityHandler}/> : <AiOutlineMenu className='w-6 h-6' onClick={navVisibilityHandler}/>}</div>
         <div className={`${navIsVisible ? "right-0" : "-right-full"} transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static  gap-x-9 items-center`}>
