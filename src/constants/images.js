5bfc644078b0f0ee2b989a6e074d5029b979393d
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-06-17 17:13:15 +0200

    dynamic post on homepage

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,16 @@ import Logo from "../assets/Logo.png";
 import HeroImage from "../assets/HeroImage.jpg";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.png";
-import CTAImage from "../assets/CtaImage.jpg"
+import CTAImage from "../assets/CtaImage.jpg";
+const noImagePost = '/images/noImage.jpg';
+const noUserImagePost = '/images/noUser';
 const images = {
     Logo, 
     HeroImage,
     Post1Image,
     PostProfileImage,
-    CTAImage
+    CTAImage,
+    noImagePost,
+    noUserImagePost
 };
 export default images;
