944bd6787a4db9512950e34b0af4b36c601f485f
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-07-09 18:04:21 +0200

    country names in italian

--- src/pages/database/TeamsDatabase.jsx
+++ src/pages/database/TeamsDatabase.jsx
@@ -5,15 +5,28 @@ import mapData from '../../data/countries/countries.json'
 import "leaflet/dist/leaflet.css";
 import '../../pages/database/TeamsDatabase.css';
 const TeamsDatabase = () => {
-    countryStyle = {
-        fillColor: "#c1f5ca",
-
-    }
+  const countryStyle = {
+    fillColor: '#90EE90',
+    color: 'green',
+    weight: 1
+  };
+  const onEachCountry = (feature, layer) => {
+    const countryName = feature.properties.ADMIN;
+    layer.on({
+      mouseover: (event) => {
+        layer.openPopup();
+      },
+      mouseout: (event) => {
+        layer.closePopup();
+      }
+    });
+    layer.bindPopup(countryName);
+  };
   return (
       <MainLayout>
-        <h1 className='text-center font-roboto'>Un mondo di nazionali!</h1>
+        <h1 className='font-roboto text-3xl text-center font-bold text-primary mb-4 md:text-5xl lg:text-4xl xl:text-5xl lg:max-w-[540]"'>Un mondo di nazionali!</h1>
         <MapContainer style={{height:"80vh"}} zoom={2} center={[20, 100]}>
-            <GeoJSON style={this.countryStyle} data={mapData.features}/>
+            <GeoJSON style={countryStyle} data={mapData} onEachFeature={onEachCountry}/>
         </MapContainer>
       </MainLayout>
   )
