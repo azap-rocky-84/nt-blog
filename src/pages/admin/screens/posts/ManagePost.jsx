1a6134c4933b8c4df12d02f8a751cd052e7ae9c2
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-08-09 16:08:55 +0200

    tiptap dynamic post content

--- src/pages/admin/screens/posts/ManagePost.jsx
+++ src/pages/admin/screens/posts/ManagePost.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { deletePost, getAllPosts } from '../../../../services/index/posts';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { images, stables } from '../../../../constants';
-import Pagination from '../../../../components/Pagination';
-import { useEffect } from 'react';
-import { toast } from 'react-hot-toast';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { deletePost, getAllPosts } from "../../../../services/index/posts";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { images, stables } from "../../../../constants";
+import Pagination from "../../../../components/Pagination";
+import { useEffect } from "react";
+import { toast } from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 let isFirstRun = true;
 
 const ManagePost = () => {
@@ -14,156 +14,228 @@ const ManagePost = () => {
   const userState = useSelector((state) => state.user);
   const [searchKeyword, setSearchKeyword] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const {data:postsData, isLoading, isFetching, refetch} = useQuery({
+  const {
+    data: postsData,
+    isLoading,
+    isFetching,
+    refetch,
+  } = useQuery({
     queryFn: () => getAllPosts(searchKeyword, currentPage),
     queryKey: ["posts"],
   });
-  const { mutate: mutateDeletePost, isLoading: isLoadingDeletePost } = useMutation({
-    mutationFn: ({ slug, token }) => {
-      return deletePost({
-        slug,
-        token,
-      });
-    },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries(["posts"]);
-      toast.success("Post eliminato correttamente");
-    },
-    onError: (error) => {
-      toast.error(error.message);
-      console.log(error);
-    },
-  });
+  const { mutate: mutateDeletePost, isLoading: isLoadingDeletePost } =
+    useMutation({
+      mutationFn: ({ slug, token }) => {
+        return deletePost({
+          slug,
+          token,
+        });
+      },
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["posts"]);
+        toast.success("Post eliminato correttamente");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+        console.log(error);
+      },
+    });
   useEffect(() => {
-    if(isFirstRun){
-        isFirstRun = false;
-        return;
+    if (isFirstRun) {
+      isFirstRun = false;
+      return;
     }
     refetch();
   }, [refetch, currentPage]);
-  const searchKeywordHandler = (e) =>{
-    const {value} = e.target;
+  const searchKeywordHandler = (e) => {
+    const { value } = e.target;
     setSearchKeyword(value);
   };
-  const submitSearchKeywordHandler = (e) =>{
+  const submitSearchKeywordHandler = (e) => {
     e.preventDefault();
     setCurrentPage(1);
     refetch();
   };
-  const deletePostHandler = ({slug, token}) => {
-    mutateDeletePost({slug, token});
-  }
+  const deletePostHandler = ({ slug, token }) => {
+    mutateDeletePost({ slug, token });
+  };
   return (
     <div>
-      <h1 className='text-2xl font-semibold'>Gestisci post</h1>
-        <div className="w-full px-4 mx-auto">
-          <div className="py-8">
-        <div className="flex flex-row justify-between w-full mb-1 sm:mb-0">
-            <h2 className="text-2xl leading-tight">
-                Users
-            </h2>
+      <h1 className="text-2xl font-semibold">Gestisci post</h1>
+      <div className="mx-auto w-full px-4">
+        <div className="py-8">
+          <div className="mb-1 flex w-full flex-row justify-between sm:mb-0">
+            <h2 className="text-2xl leading-tight">Users</h2>
             <div className="text-end">
-                <form onSubmit={submitSearchKeywordHandler} className="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
-                    <div className=" relative ">
-                        <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Titolo del post..." onChange={searchKeywordHandler} value={searchKeyword}/>
-                        </div>
-                        <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                            Filter
-                        </button>
-                    </form>
+              <form
+                onSubmit={submitSearchKeywordHandler}
+                className="flex w-3/4 max-w-sm flex-col justify-center space-y-3 md:w-full md:flex-row md:space-x-3 md:space-y-0"
+              >
+                <div className=" relative ">
+                  <input
+                    type="text"
+                    id='"form-subscribe-Filter'
+                    className=" w-full flex-1 appearance-none rounded-lg border border-gray-300 border-transparent bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600"
+                    placeholder="Titolo del post..."
+                    onChange={searchKeywordHandler}
+                    value={searchKeyword}
+                  />
                 </div>
+                <button
+                  className="flex-shrink-0 rounded-lg bg-purple-600 px-4 py-2 text-base font-semibold text-white shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+                  type="submit"
+                >
+                  Filter
+                </button>
+              </form>
             </div>
-            <div className="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
-                <div className="inline-block min-w-full overflow-hidden rounded-lg shadow">
-                    <table className="min-w-full leading-normal">
-                        <thead>
-                            <tr>
-                                <th scope="col" className="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                                    Titolo
-                                </th>
-                                <th scope="col" className="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                                    Categoria
-                                </th>
-                                <th scope="col" className="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                                   Data
-                                </th>
-                                <th scope="col" className="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                                    Tags
-                                </th>
-                                <th scope="col" className="px-5 py-3 text-sm font-normal text-left text-gray-800 uppercase bg-white border-b border-gray-200">
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                          {isLoading || isFetching ? (
-                            <tr>
-                              <td colSpan={5} className='text-center py-10 w-full'>
-                                 Carico...
-                              </td>
-                            </tr>
-                          ) : postsData?.data?.length === 0 ? (
-                            <tr>
-                              <td colSpan={5} className='text-center py-10 w-full'>
-                                 Nessun post trovato nel database
-                              </td>
-                            </tr>
-                          ) : (
-                            postsData?.data.map((post) => (
-                              <tr>
-                                <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                                    <div className="flex items-center">
-                                        <div className="flex-shrink-0">
-                                            <a href="/" className="relative block">
-                                                <img alt="profil" src={post?.image ? stables.UPLOAD_FOLDER_BASE_URL + post?.image : images.noImagePost} className="mx-auto object-cover rounded-lg w-10 aspect-square"/>
-                                            </a>
-                                        </div>
-                                        <div className="ml-3">
-                                            <p className="text-gray-900 whitespace-no-wrap">
-                                                {post.title}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                                    <p className="text-gray-900 whitespace-no-wrap">
-                                        {post.categories.length>0 ? post.categories[0] : "Categoria non inserita"}
-                                    </p>
-                                </td>
-                                <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                                    <p className="text-gray-900 whitespace-no-wrap">
-                                        {new Date(post.createdAt).toLocaleDateString("en-US", {
-                                          day: "numeric",
-                                          month: "short",
-                                          year: "numeric",
-                                        })}
-                                    </p>
-                                </td>
-                                <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                                   <div className='flex gap-x-2 flex-wrap'>
-                                        {post.tags.length>0 ? post.tags.map((tag, index) => (
-                                          <p key={index}>{tag}{post.tags.length-1 !== index && ','}</p>
-                                        )) : "Tags non inseriti"}
-                                   </div>
-                                </td>
-                                <td className="px-5 py-5 text-sm bg-white border-b border-gray-200 space-x-5">
-                                     <button disabled={isLoadingDeletePost} type="button" className='text-red-600 hover:text-red-900 disabled:opacity-70 disabled:cursor-not-allowed' onClick={() => {deletePostHandler({slug:post?.slug, token: userState.userInfo.token})}}>Cancella</button>
-                                    <Link to={`/admin/posts/manage/edit/${post?.slug}`} className="text-green-600 hover:text-green-900">
-                                        Modifica
-                                    </Link>
-                                </td>
-                            </tr>
-                            ))
-                          )}
-                        </tbody>
-                    </table>
-                    {!isLoading && <Pagination onPageChange={(page) => setCurrentPage(page)} currentPage={currentPage} totalPageCount={JSON.parse(postsData?.headers?.['x-totalpagecount'])}/> }
-                </div>
+          </div>
+          <div className="-mx-4 overflow-x-auto px-4 py-4 sm:-mx-8 sm:px-8">
+            <div className="inline-block min-w-full overflow-hidden rounded-lg shadow">
+              <table className="min-w-full leading-normal">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white px-5 py-3 text-left text-sm font-normal uppercase text-gray-800"
+                    >
+                      Titolo
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white px-5 py-3 text-left text-sm font-normal uppercase text-gray-800"
+                    >
+                      Categoria
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white px-5 py-3 text-left text-sm font-normal uppercase text-gray-800"
+                    >
+                      Data
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white px-5 py-3 text-left text-sm font-normal uppercase text-gray-800"
+                    >
+                      Tags
+                    </th>
+                    <th
+                      scope="col"
+                      className="border-b border-gray-200 bg-white px-5 py-3 text-left text-sm font-normal uppercase text-gray-800"
+                    ></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {isLoading || isFetching ? (
+                    <tr>
+                      <td colSpan={5} className="w-full py-10 text-center">
+                        Carico...
+                      </td>
+                    </tr>
+                  ) : postsData?.data?.length === 0 ? (
+                    <tr>
+                      <td colSpan={5} className="w-full py-10 text-center">
+                        Nessun post trovato nel database
+                      </td>
+                    </tr>
+                  ) : (
+                    postsData?.data.map((post) => (
+                      <tr>
+                        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                              <a href="/" className="relative block">
+                                <img
+                                  alt="profil"
+                                  src={
+                                    post?.photo
+                                      ? stables.UPLOAD_FOLDER_BASE_URL +
+                                        post?.photo
+                                      : images.noImagePost
+                                  }
+                                  className="mx-auto aspect-square w-10 rounded-lg object-cover"
+                                />
+                              </a>
+                            </div>
+                            <div className="ml-3">
+                              <p className="whitespace-no-wrap text-gray-900">
+                                {post.title}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                          <p className="whitespace-no-wrap text-gray-900">
+                            {post.categories.length > 0
+                              ? post.categories[0]
+                              : "Categoria non inserita"}
+                          </p>
+                        </td>
+                        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                          <p className="whitespace-no-wrap text-gray-900">
+                            {new Date(post.createdAt).toLocaleDateString(
+                              "en-US",
+                              {
+                                day: "numeric",
+                                month: "short",
+                                year: "numeric",
+                              }
+                            )}
+                          </p>
+                        </td>
+                        <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                          <div className="flex flex-wrap gap-x-2">
+                            {post.tags.length > 0
+                              ? post.tags.map((tag, index) => (
+                                  <p key={index}>
+                                    {tag}
+                                    {post.tags.length - 1 !== index && ","}
+                                  </p>
+                                ))
+                              : "Tags non inseriti"}
+                          </div>
+                        </td>
+                        <td className="space-x-5 border-b border-gray-200 bg-white px-5 py-5 text-sm">
+                          <button
+                            disabled={isLoadingDeletePost}
+                            type="button"
+                            className="text-red-600 hover:text-red-900 disabled:cursor-not-allowed disabled:opacity-70"
+                            onClick={() => {
+                              deletePostHandler({
+                                slug: post?.slug,
+                                token: userState.userInfo.token,
+                              });
+                            }}
+                          >
+                            Cancella
+                          </button>
+                          <Link
+                            to={`/admin/posts/manage/edit/${post?.slug}`}
+                            className="text-green-600 hover:text-green-900"
+                          >
+                            Modifica
+                          </Link>
+                        </td>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
+              {!isLoading && (
+                <Pagination
+                  onPageChange={(page) => setCurrentPage(page)}
+                  currentPage={currentPage}
+                  totalPageCount={JSON.parse(
+                    postsData?.headers?.["x-totalpagecount"]
+                  )}
+                />
+              )}
             </div>
+          </div>
         </div>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 };
 
-export default ManagePost
+export default ManagePost;
--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,70 +1,106 @@
-import React, { useState } from 'react'
-import {Link, useParams} from 'react-router-dom';
-import MainLayout from '../../components/MainLayout'
-import BreadCrumbs from '../../components/BreadCrumbs'
-import { images, stables } from '../../constants'
-import SuggestedPosts from './container/SuggestedPosts'
-import CommentsContainer from '../../components/comments/CommentsContainer'
-import SocialShareButtons from '../../components/SocialShareButtons'
-import { useQuery } from '@tanstack/react-query'
-import { getAllPosts, getSinglePost } from '../../services/index/posts'
-import ArticleDetailSkeleton from './components/ArticleDetailSkeleton';
-import ErrorMessage from '../../components/ErrorMessage';
-import { useSelector } from 'react-redux';
-import parseJsonToHtml from '../../utils/parseJsonToHtml';
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import MainLayout from "../../components/MainLayout";
+import BreadCrumbs from "../../components/BreadCrumbs";
+import { images, stables } from "../../constants";
+import SuggestedPosts from "./container/SuggestedPosts";
+import CommentsContainer from "../../components/comments/CommentsContainer";
+import SocialShareButtons from "../../components/SocialShareButtons";
+import { useQuery } from "@tanstack/react-query";
+import { getAllPosts, getSinglePost } from "../../services/index/posts";
+import ArticleDetailSkeleton from "./components/ArticleDetailSkeleton";
+import ErrorMessage from "../../components/ErrorMessage";
+import { useSelector } from "react-redux";
+import parseJsonToHtml from "../../utils/parseJsonToHtml";
+import Editor from "../../components/editor/Editor";
 
 const ArticleDetailPage = () => {
-  const {slug} = useParams();
-  const userState = useSelector((state => state.user));
+  const { slug } = useParams();
+  const userState = useSelector((state) => state.user);
   const [breadCrumbsData, setBreadCrumbsData] = useState([]);
   const [body, setBody] = useState(null);
-  const {data, isLoading, isError} = useQuery({
-    queryFn: () => getSinglePost({slug}),
-    queryKey: ['blog', slug],
+  const { data, isLoading, isError } = useQuery({
+    queryFn: () => getSinglePost({ slug }),
+    queryKey: ["blog", slug],
     onSuccess: (data) => {
       setBreadCrumbsData([
-        {name: "Home", link: '/'},
-        {name: "Blog", link: '/blog'},
-        {name: "Article Title", link: `/blog/${data.slug}`},
+        { name: "Home", link: "/" },
+        { name: "Blog", link: "/blog" },
+        { name: "Article Title", link: `/blog/${data.slug}` },
       ]);
       setBody(parseJsonToHtml(data?.body));
     },
   });
-  const {data:postsData} = useQuery({
+  const { data: postsData } = useQuery({
     queryFn: () => getAllPosts(),
     queryKey: ["posts"],
   });
-   return (
+  return (
     <MainLayout>
       {isLoading ? (
-        <ArticleDetailSkeleton/>
-      ) : isError ? <ErrorMessage message="Impossibile caricare i dettagli del post"/> : (
-        <section className='container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start'>
-           <article className='flex-1'>
-            <BreadCrumbs data={breadCrumbsData}/>
-            <img className='rounded-xl w-full' src={data?.photo ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo : images.noImagePost} alt={data?.title} />
-            <div className='mt-4 flex gap-2'>
-            {data?.categories.map((category) => (
-              <Link to={`/blog?category=${category.name}`} className='text-primary text-sm font-roboto inline-block md:text-base'>
-            {category.name}
-           </Link>
-            ))}
+        <ArticleDetailSkeleton />
+      ) : isError ? (
+        <ErrorMessage message="Impossibile caricare i dettagli del post" />
+      ) : (
+        <section className="container mx-auto flex max-w-5xl flex-col px-5 py-5 lg:flex-row lg:items-start lg:gap-x-5">
+          <article className="flex-1">
+            <BreadCrumbs data={breadCrumbsData} />
+            <img
+              className="w-full rounded-xl"
+              src={
+                data?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo
+                  : images.noImagePost
+              }
+              alt={data?.title}
+            />
+            <div className="mt-4 flex gap-2">
+              {data?.categories.map((category) => (
+                <Link
+                  to={`/blog?category=${category.name}`}
+                  className="inline-block font-roboto text-sm text-primary md:text-base"
+                >
+                  {category.name}
+                </Link>
+              ))}
             </div>
-           <h1 className='text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]'>{data?.title}</h1>
-           <div className='mt-4 prose prose-sm sm:prose-base'>{body}</div>
-           <CommentsContainer comments={data?.comments} className='mt-10' logginedUserId={userState?.userInfo?._id} postSlug={slug}/>
-           </article>
-           <div>
-           <SuggestedPosts header="Ultimi articoli" posts={postsData?.data} tags={data?.tags} className='mt-8 lg:mt-0 lg:max-w-xs'/>
-           <div className='mt-7'>
-              <h2 className='font-roboto font-medium text-colortext mb-4 md:text-xl'>Condividi: </h2>
-              <SocialShareButtons url={encodeURI(window.location.href)} title={encodeURIComponent(data?.title)}/>
-           </div>
-           </div>
+            <h1 className="mt-4 font-roboto text-xl font-medium text-dark-hard md:text-[26px]">
+              {data?.title}
+            </h1>
+            <div className="w-full">
+              {!isLoading && !isError && (
+                <Editor content={data?.body} editable={false} />
+              )}
+            </div>
+            <CommentsContainer
+              comments={data?.comments}
+              className="mt-10"
+              logginedUserId={userState?.userInfo?._id}
+              postSlug={slug}
+            />
+          </article>
+          <div>
+            <SuggestedPosts
+              header="Ultimi articoli"
+              posts={postsData?.data.slice(0, 3)}
+              tags={data?.tags}
+              className="mt-8 lg:mt-0 lg:max-w-xs"
+            />
+            <div className="mt-7">
+              <h2 className="mb-4 font-roboto font-medium text-colortext md:text-xl">
+                Condividi:{" "}
+              </h2>
+              <SocialShareButtons
+                url={encodeURI(window.location.href)}
+                title={encodeURIComponent(data?.title)}
+              />
+            </div>
+          </div>
         </section>
-      )}; 
+      )}
+      ;
     </MainLayout>
-  )
-}
+  );
+};
 
-export default ArticleDetailPage
+export default ArticleDetailPage;
