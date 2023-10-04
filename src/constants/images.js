d01dd2edec45df8c52e71f8006527fd363865690
Alfonso Zappia <72659505+alfonso-zappia08@users.noreply.github.com>
2023-10-04 11:55:50 +0200

    new background video

--- src/constants/images.js
+++ src/constants/images.js
@@ -5,7 +5,6 @@ import PostProfileImage from "../assets/posts/post-profile.png";
 import CTAImage from "../assets/CtaImage.jpg";
 import WhiteLogo from "../assets/WhiteLogo.png";
 import BackgroundVideo from "../assets/BackgroundVideo.mp4";
-import ProofQuoter from "../assets/WhatsApp Image 2023-06-23 at 12.47.05.jpeg"
 const noImagePost = "/images/noImage.jpg";
 const noUserImagePost = "/images/noUser";
 const images = {
@@ -18,6 +17,5 @@ const images = {
   noImagePost,
   noUserImagePost,
   BackgroundVideo,
-  ProofQuoter
 };
 export default images;
